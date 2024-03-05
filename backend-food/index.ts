04d4cb2d53083bd1efaf3685f54e5ef6114e9b68
z0l0git <zolbayar.in@gmail.com>
2024-03-05 12:34:38 +0800

    teneg nnamu

--- backend-food/index.ts
+++ backend-food/index.ts
@@ -1,8 +1,16 @@
 import { connectDb } from "./db";
 import express from "express";
 import cors from "cors";
+import userRouter from "./routes/userRouter";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(router);
+connectDb();
+
+app.use(userRouter);
+
+app.listen(4000, () => {
+  console.log("http://localhost:4000");
+});
